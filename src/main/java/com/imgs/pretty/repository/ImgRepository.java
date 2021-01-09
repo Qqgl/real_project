ca1eef19be809cabd0adcad3bd07bceafa735db8
qgl <1179837471@qq.com>
2021-01-09 16:40:25 +0800

    添加mongodb

--- src/main/java/com/imgs/pretty/repository/ImgRepository.java
+++ src/main/java/com/imgs/pretty/repository/ImgRepository.java
@@ -4,6 +4,10 @@ import com.imgs.pretty.entity.Image;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ImgRepository extends MongoRepository<Image,String> {
+
+    List<Image> findByIdIn(List<String>ids);
 }
--- src/test/java/com/imgs/pretty/service/ImgsServiceTest.java
+++ src/test/java/com/imgs/pretty/service/ImgsServiceTest.java
@@ -11,22 +11,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
-
-import static org.junit.Assert.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ImgsServiceTest {
+
+    private static ExecutorService pool = Executors.newCachedThreadPool();
+
+    private static CountDownLatch countDownLatch = new CountDownLatch(10);
 
     @Autowired
     private ImgRepository imgRepository;
 
     @Test
     public void save(){
-        Image image = new Image().setContent(new Binary("hello word".getBytes()))
-                .setContentType("string")
-                .setName("test")
+        Image image = new Image().setContent(new Binary("hello word2".getBytes()))
+                .setContentType("jpg")
+                .setName("test2")
                 .setUploadTime(TimeUtil.getCurTime(LocalDateTime.now()))
                 .setSize(100);
         Image save = imgRepository.save(image);
@@ -37,6 +45,31 @@ public class ImgsServiceTest {
     public void findOne(){
         Optional<Image> optional = imgRepository.findById("5fdafdf5ef8a56167c086461");
         System.out.println(optional.get());
+    }
+
+    @Test
+    public void findByIds(){
+        List<Image> imageList = imgRepository.findByIdIn(Arrays.asList("5fdc658bef8a5628f827381f", "5fdafdf5ef8a56167c086461"));
+        System.out.println(imageList);
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+
+            pool.execute(()->{
+                countDownLatch.countDown();
+                try {
+                    countDownLatch.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Optional<Image> optional = imgRepository.findById("5fdafdf5ef8a56167c086461");
+                System.out.println(optional.get());
+            });
+        }
+
+        Thread.sleep(2_000);
     }
 
 }
